095a96ff3a6cc3c344655eed518ab7c9a7fd7e0e
Lucas Fernandes <lucasf.goiano347@gmail.com>
2022-04-16 15:38:52 -0300

    [MONGOOSE] - Update schema Company

--- src/models/Company.js
+++ src/models/Company.js
@@ -4,6 +4,7 @@ const companySchema = new mongoose.Schema({
   id: { type: String },
   nome: { type: String, required: true },
   endereco: { type: String, required: true },
+  CNPJ: { type: String, required: true }
 });
 
 const companys = mongoose.model("companys", companySchema);
